8433da6f272b35d8e5e6b485b4b9ea1e6edf50b8
arsenyeganyan <125376725+arsenyeganyan@users.noreply.github.com>
2024-01-30 19:40:04 +0400

    try catch.

--- js/login.js
+++ js/login.js
@@ -1,30 +1,38 @@
 var form = document.getElementById('login__form');
 
 form.addEventListener('submit', async function(event) {
-    event.preventDefault();
-    var username = form.username.value;
-    var password = form.password.value;
-
-    var url = '';
-    const response = await fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            username: username,
-            password: password,
-        }),
-    })
     
-    const responseData = await response.json(); 
-    const token = responseData.token;
-    if (token){
-        localStorage.setItem('token', token);
-        window.location.href = '/home.html';
-    }
-    else {
-        var error = document.getElementById('error__msg');
-        error.innerHTML = 'Bad username or password';
+    try {
+        event.preventDefault();
+        var username = form.username.value;
+        console.log(username);
+        var password = form.password.value;
+        console.log(password);
+
+        var url = '/api/auth/login';
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+            }),
+        });
+        
+        const responseData = await response.json();
+        console.log(responseData);
+        const token = responseData.token;
+
+        if (token){
+            localStorage.setItem('token', token);
+            window.location.href = '/';
+        } else {
+            var error = document.getElementById('error__msg');
+            error.innerHTML = 'Bad username or password';
+        }
+    } catch(err) {
+        console.error(error);
     }
 });
