7ea80c5cbb12a4fbf80c21c5084d99114aa1b55e
Aren2020 <aren.allahverdyan@gmail.com>
2024-02-15 20:40:38 +0400

    password change

--- js/home.js
+++ js/home.js
@@ -1,7 +1,3 @@
-if (!localStorage.getItem('toke')) {
-    console.log('redirect');
-}
-
 var sidebar = document.getElementById('sidebar');
 var user_edit = document.getElementById('user__edit');
 var edit_form = document.getElementById('edit__form');
@@ -12,6 +8,62 @@ var new_chat = document.getElementById('new__chat');
 var sidebar_settings = document.getElementById('sidebar__settings');
 var chat_box_header_settings = document.getElementById('chat__box__header__settings');
 var profile__picture__label =  document.getElementById('profile__picture__label');
+var change_password = document.getElementById('change__password');
+var change_password_form = document.getElementById('change__password__form');
+
+change_password_form.addEventListener('submit', async function(event){
+    event.preventDefault();
+    var oldPassword = change_password_form.oldPassword.value;
+    var newPassword = change_password_form.newPassword.value;
+    var csrf_token = edit_form._csrf.value;
+
+    if (oldPassword == newPassword){
+        var url = '/api/edit/password';
+        
+        var response = await fetch(url, {
+            headers: {
+                'X-CSRF-Token': csrf_token,
+                'Authorization': `Token ${localStorage.getItem('token')}`
+            },
+            method: 'POST',
+            body: {'new_password': newPassword},
+        });
+
+        var notification_sound = new Audio('../sounds/new-message-2-125765.mp3');
+        await notification_sound.play();
+
+        if(!response.ok) {
+            error_message.style.display = 'flex';
+            throw new Error("An error occured", response.status);
+        }
+        
+        const responseData = await response.json();
+        console.log(responseData);
+
+        document.getElementById('error__msg__in__change__password').innerHTML = '';
+        document.getElementById('error__msg__in__change__password').style.display = 'none';
+
+        error_message.style.display = 'none';
+        success_message.style.display = 'flex';
+        return;
+    }
+    else {
+        document.getElementById('error__msg__in__change__password').style.display = 'block';
+    }
+
+})
+
+// document.getElementById
+
+document.getElementById('settings__change__password').addEventListener('click', function(){
+    sidebar.style.display = 'none';
+    change_password.style.display = 'flex';  
+})
+
+document.getElementById('change__password__close').addEventListener('click', function(){
+    change_password.style.display = 'none';
+    sidebar.style.display = 'flex'; 
+})
 
 document.getElementById('sidebar__settings__icon').addEventListener('click', function(){
     if (sidebar_settings.style.display == 'flex'){
@@ -36,10 +88,13 @@ document.getElementById('new__chat__close').addEventListener('click', function()
     sidebar.style.display = 'flex'
 })
 
-document.getElementById('new__chat__icon').addEventListener('click', function() {
+function new__chat__diplay() {
     new_chat.style.display = 'flex';
     sidebar.style.display = 'none';
-})
+}
+
+document.getElementById('new__chat__icon').addEventListener('click', new__chat__diplay);
+document.getElementById('settings__new__chat').addEventListener('click', new__chat__diplay);
 
 document.getElementById('user__edit__close').addEventListener('click', function(){
     var profile_picture_url = localStorage.getItem('profile_picture');
@@ -128,3 +183,8 @@ for(var i = 0; i < cancel_icons.length; i++){
         error_message.style.display = 'none';
     }) 
 }
+
+document.getElementById('log__out').addEventListener('click', function(){
+    localStorage.removeItem('token');
+    window.location.href = '/login.html';
+})
